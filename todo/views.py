90568796b0b61311db60849ae21cdfadfb3a48fd
LitzN <alitzianedyalkova@hotmail.co.uk>
2020-12-31 15:29:02 +0000

    Added edit page

--- todo/views.py
+++ todo/views.py
@@ -14,13 +14,15 @@ def get_todo_list(request):
 
 def add_item(request):
     if request.method == "POST":
-        name = request.POST.get('item_name')
-        done = 'done' in request.POST
-        Item.objects.create(name=name, done=done)
-
+        form = ItemForm(request.POST)
+        if form.is_valid():
+            form.save()
         return redirect('get_todo_list')
     form = ItemForm()
     context = {
         'form': form
     }
     return render(request, 'todo/add_item.html', context)
+
+def edit_item(request, item_id):
+    return render(request, 'todo/edit_item.html')
